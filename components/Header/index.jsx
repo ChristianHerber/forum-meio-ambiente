f160631029c3dc4ded779d728161a4807a52230c
Christian Herber dos Santos <christianh@PC-1013592.pmtga.dom>
2023-05-10 15:44:58 -0400

    criando components

---
+++ components/Header/index.jsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/Home.module.css'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <h1>1º Fórum Municipal do <strong>Meio Ambiente</strong></h1>
+            <p>Dias 05 e 06 de Junho de 2023</p>
+            <p>Auditório do Centro Cultural Pedro Alberto Tayano Filho</p>
+        </header>
+    )
+}
+
+export default Header
