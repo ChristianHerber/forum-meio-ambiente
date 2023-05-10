37dbce92cc05ebe5633835e1944ce45d80f94a1b
Christian Herber <christianherbersantos@gmail.com>
2023-05-10 17:11:58 -0400

    Ajuste no título

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -3,7 +3,8 @@ import styles from './style.module.css'
 const Header = () => {
     return (
         <header className={styles.header}>
-            <h1>1º Fórum Municipal do <strong>Meio Ambiente</strong></h1>
+            <h1>1º Fórum Municipal do</h1>
+            <h1 className={styles.subtitle}>Meio Ambiente</h1>
             <p>Dias 05 e 06 de Junho de 2023</p>
             <p>Auditório do Centro Cultural Pedro Alberto Tayano Filho</p>
         </header>
