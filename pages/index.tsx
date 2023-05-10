e351b05b9e6c31a4f8cd60d28949a20b58021a97
Christian Herber <christianherbersantos@gmail.com>
2023-05-10 17:05:35 -0400

    chamando os components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import Description from '../components/Description'
+import Topic from '../components/Topic'
+import Footer from '../components/Footer'
+import Button from '../components/Button'
 
 const Home: NextPage = () => {
   return (
@@ -16,26 +20,22 @@ const Home: NextPage = () => {
       <Header />
 
       <main className={styles.main}>
-        <Image src="/assets/img/logo.png" className={styles.forumLogo} alt="1º Fórum Municipal de Meio Ambiente" width={200} height={200} />
-        <p>O primeiro Fórum Municipal do Meio Ambiente de Tangará da Serra é promovido pela Prefeitura Municipal, por meio da Secretaria Municipal de Meio Ambiente.</p>
-        <p>O evento acontecerá entre os dias 05 e 06 de junho de 2023 no auditório do Centro Cultural Pedro Alberto Tayano Filho.</p>
-        <p>O fórum contará com palestras, mesas redondas e debates acerca dos principais temas que envolvem a gestão ambiental do nosso município.</p>
-        <p>Serão palestrantes convidados da SEMA, ANA, Polícia Militar de Proteção Ambiental, UNEMAT, entre outros.
-        As inscrições são limitadas e serão abertas no dia 17 de maio de 2023, podendo ser realizadas no link abaixo.</p>
 
-        <h2>Link para Inscrição</h2>
-        <a href="#" className={styles.btn}>Fazer Inscrição</a>
+        <Description />
 
-        <h2>Presenças Confirmadas</h2>
+        <Topic htitle="Link para Inscrição"/>
+        <Button LinkHref='https://forumsemmeatga.com.br' LinkTarget="_blank" LinkTitle="Fazer Inscrição"/>
 
-        <h2>Fotos</h2>
+        <Topic htitle="Presenças Confirmadas"/>
+        
+        <Topic htitle="Fotos"/>
 
-        <h2>Cronograma do Evento</h2>
+        <Topic htitle="Cronograma do Evento"/>
+
       </main>
 
-      <footer className={styles.footer}>
+      <Footer />
 
-      </footer>
     </div>
   )
 }
