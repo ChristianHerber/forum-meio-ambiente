cf0d8e7b918cd1dfea17834f63f6e129ac0e9744
Christian Herber <christianherbersantos@gmail.com>
2023-05-16 08:19:11 -0400

    Finalizando o footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,10 +1,26 @@
 import styles from './style.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
     return (
+        <>
         <footer className={styles.footer}>
+            <div className={styles.footer__client}>
+                <Image src='/assets/img/brasao-p.png' width={66} height={64}/>
+                <div className={styles.footer__description}>
+                    <p><strong>Prefeitura Municipal de Tangará da Serra</strong></p>
+                    <p><strong>Secretaria Municipal de Meio Ambiente</strong></p>
+                </div>
+            </div>
 
+            <div className={styles.footer__copy}>
+                <small className={styles.footer__author}>
+                    <span>Christian Herber Santos</span> <strong>Analista Desenvolvedor de Sistemas</strong>
+                </small>
+            </div>
         </footer>
+
+        </>
     )
 }
 
