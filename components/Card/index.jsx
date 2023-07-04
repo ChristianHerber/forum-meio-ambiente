f32dbe930d6f77faffc0da08a4ac8ae26db69387
Christian Herber <christianherbersantos@gmail.com>
2023-07-04 09:03:10 -0400

    add slides

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -1,15 +1,18 @@
+import Link from 'next/link'
 import styles from './style.module.css'
 import Image from 'next/image'
 
-const Card = ({cardName, cardTitle, cardPhoto, cardPhotoAlt}) => {
+const Card = ({cardName, cardTitle, cardPhoto, cardPhotoAlt, cardLink}) => {
     return (
+        <Link href={cardLink} target='_blank'>
         <div className={styles.card}>
-            <Image src={cardPhoto} alt={cardPhotoAlt} width={320} height={380} />
+            <Image src={cardPhoto} alt={cardPhotoAlt} width={318} height={170} />
             <div className={styles.card_body}>
                 <h4 className={styles.card_name}>{cardName}</h4>
                 <p className={styles.card_title}>{cardTitle}</p>
             </div>
         </div>
+        </Link>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,15 +32,46 @@ const Home: NextPage = () => {
           <Button LinkHref='https://www.sympla.com.br/1-forum-municipal-do-meio-ambiente-de-tangara-da-serra__1990357' LinkTarget="_blank" LinkTitle="Fazer Inscrição"/>
         </Container>
 
-        {/* <Container>
-          <Topic htitle="Presenças Confirmadas" anchor='presenca'/>
+        <Container>
+          <Topic htitle="Palestras realizadas" anchor='presenca'/>
           <div className={styles.card_container}>
-            <Card cardPhoto="/assets/img/profile-photo.jpg" cardPhotoAlt="" cardName="Taline de Almeida" cardTitle="Bióloga" />
-            <Card cardPhoto="/assets/img/profile-photo.jpg" cardPhotoAlt="" cardName="Alexandra Silva" cardTitle="Engenheira" />
-            <Card cardPhoto="/assets/img/profile-photo.jpg" cardPhotoAlt="" cardName="Maria Júlia" cardTitle="Eng Sanitarista" />
-            <Card cardPhoto="/assets/img/profile-photo.jpg" cardPhotoAlt="" cardName="Yasmin Santos" cardTitle="Bióloga" />
+            <Card
+              cardLink="https://s3.sa-east-1.amazonaws.com/otimize-edoc/edoc_1255/2023/06/28/3CC7E2BF5CCF249E0B905A25/Apresentacao_3_CIBM_Incendios_Florestais_Vegetacao.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY2AOIST6YOOX2GWQ%2F20230704%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230704T123725Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=4b0c07719a59ef445142bccdda53ebd909e708ac6706a7a6da5a12e54abe23b3"
+              cardPhoto="/assets/img/slide-palestra.jpg"
+              cardPhotoAlt="Panoragma de Incêncdio Florestal"
+              cardName="Campos Filho"
+              cardTitle="CAP QOBM"
+            />
+            <Card
+              cardLink="https://s3.sa-east-1.amazonaws.com/otimize-edoc/edoc_1255/2023/06/28/3CC7E2BF5CCF249E0B905A25/Apresentacao_SEMA_do_PGRS_Digital.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY2AOIST6YOOX2GWQ%2F20230704%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230704T123725Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=4b9ae0e25e574afe219ba3b2a23ef662c0676969e75cfa465d6e5fb92bb03c54"
+              cardPhoto="/assets/img/slide-palestra-1.jpg"
+              cardPhotoAlt="PGRS Digital Módulo de Gestão Pública"
+              cardName="Giselle Lima"
+              cardTitle="Chefe UPPE"
+            />
+            <Card
+              cardLink="https://s3.sa-east-1.amazonaws.com/otimize-edoc/edoc_1255/2023/06/28/3CC7E2BF5CCF249E0B905A25/Palestra_Responsabilizacao_ambiental_Apresentacao.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY2AOIST6YOOX2GWQ%2F20230704%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230704T123727Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=698fe8ce4fb86e4f2ee5f13e9ccd47a6b72a44d928985a5980675fb1343c17ff"
+              cardPhoto="/assets/img/slide-palestra-2.jpg"
+              cardPhotoAlt="A Resposabilização Ambiental como Mecanismo de Proteção e Preservação do Meio Ambiente"
+              cardName="Belgrano Souza"
+              cardTitle="Analista Ambiental"
+            />
+            <Card
+              cardLink="https://s3.sa-east-1.amazonaws.com/otimize-edoc/edoc_1255/2023/06/28/3CC7E2BF5CCF249E0B905A25/PGRS_como_instrumento_da_gestao_publica.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY2AOIST6YOOX2GWQ%2F20230704%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230704T123729Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=21ba484447fadee326126af0c488fd6efa59f6376a399ec197e6a65aed00b8b3"
+              cardPhoto="/assets/img/slide-palestra-3.jpg"
+              cardPhotoAlt="PGRS Como Instrumento da Gestão Pública"
+              cardName="Aline Cossolin"
+              cardTitle="Analista Ambiental"
+            />
+            <Card
+              cardLink="https://s3.sa-east-1.amazonaws.com/otimize-edoc/edoc_1255/2023/06/28/3CC7E2BF5CCF249E0B905A25/PROGRAMA_DE_ADOCAO_DE_ESPACOS_PUBLICOS.pdf?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAY2AOIST6YOOX2GWQ%2F20230704%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230704T123735Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=437140b15c0a5f5da21b50ba65c1788eaf87dd39f6d3865ab27d3813c80178c8"
+              cardPhoto="/assets/img/slide-palestra-4.jpg"
+              cardPhotoAlt="PAEP Programa de Adoção de Espaços Públicos"
+              cardName="SEMMEA"
+              cardTitle="Tangará da Serra"
+            />
           </div>
-        </Container> */}
+        </Container>
 
         <Container>
           <Topic htitle="Fotos" anchor='foto'/>
